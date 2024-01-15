fafc75c086c0e76aa548db3400df48b7135b1155
henintsoa454 <henintsoa404@gmail.com>
2024-01-15 16:55:58 +0330

    last at school

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,13 @@ import { Outlet, Route, Router, RouterProvider, Routes } from 'react-router'
 import LoginPage from './login/LoginPage'
 import SideBar from './component/SideBar'
 import Statistique from './statistique/Statistique'
-import CRUD from './crud/CRUD'
+import Annonce from './annonce/Annonce'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-       <CRUD/>
+       <Annonce/>
   )
 }
 
