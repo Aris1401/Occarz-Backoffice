fa488563ca4fd57169c4de8eb7de57ae0c6d8dc8
SOLOFOARISON Tolotry Ny Avo Arisaina <62713580+Aris1401@users.noreply.github.com>
2024-01-29 22:26:17 +0300

    flex

--- src/components/login/LoginPage.jsx
+++ src/components/login/LoginPage.jsx
@@ -36,35 +36,37 @@ const LoginPage = () => {
       <div className="container">
         <Card>
           <div className="login-container">
-            <h3>OCCAR-Z Office</h3>
-            <div className="form-container">
-              <Form name="loginForm" form={loginForm} initialValues={{ remember: true, email: "admin@gmail.com", password: "admin" }} onFinish={onFinish}>
-                <Form.Item
-                  name="email"
-                  rules={[{ required: true, message: "Veuillez entrer votre nom d'utilisateur!" }]}
-                >
-                  <Input prefix={<UserOutlined />}  placeholder="Nom d'utilisateur" />
-                </Form.Item>
+            <Flex justify='center' align='center' vertical>
+              <h3>OCCAR-Z Office</h3>
+              <div className="form-container">
+                <Form name="loginForm" form={loginForm} initialValues={{ remember: true, email: "admin@gmail.com", password: "admin" }} onFinish={onFinish}>
+                  <Form.Item
+                    name="email"
+                    rules={[{ required: true, message: "Veuillez entrer votre nom d'utilisateur!" }]}
+                  >
+                    <Input prefix={<UserOutlined />}  placeholder="Nom d'utilisateur" />
+                  </Form.Item>
 
-                <Form.Item
-                  name="password"
-                  rules={[{ required: true, message: "Veuillez entrer votre mot de passe!" }]}
-                >
-                  <Input.Password prefix={<LockOutlined />} placeholder="Mot de passe" />
-                </Form.Item>
+                  <Form.Item
+                    name="password"
+                    rules={[{ required: true, message: "Veuillez entrer votre mot de passe!" }]}
+                  >
+                    <Input.Password prefix={<LockOutlined />} placeholder="Mot de passe" />
+                  </Form.Item>
 
-                {errorMessage &&
-                  <Alert type='error' icon={<WarningOutlined />} description={errorMessage} showIcon />
+                  {errorMessage &&
+                    <Alert type='error' icon={<WarningOutlined />} description={errorMessage} showIcon />
 
-                }
+                  }
 
-                <Form.Item>
-                  <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
-                    Se connecter
-                  </Button>
-                </Form.Item>
-              </Form>
-            </div>
+                  <Form.Item>
+                    <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+                      Se connecter
+                    </Button>
+                  </Form.Item>
+                </Form>
+              </div>
+            </Flex>
           </div>
         </Card>
       </div>
